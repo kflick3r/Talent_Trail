ab48165e02d44d476006f0014500b2d5b5ff1078
Kassidy Flick <kassidy.flick@colorado.edu>
2026-03-08 16:53:13 +0000

    Changed Name
    Changed landing_page to career_selection

--- app/.ipynb_checkpoints/career_selection_app-checkpoint.py
+++ app/.ipynb_checkpoints/career_selection_app-checkpoint.py
@@ -30,7 +30,7 @@ def get_careers():
 @app.route("/")
 def landing():
     careers = get_careers()
-    return render_template("landing_page.html", careers=careers)
+    return render_template("career_selection.html", careers=careers)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
